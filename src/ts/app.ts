00d59a36d31a7179503e3bd493cb63c264639237
Fast Junkie <fast-junkie@pm.me>
2023-09-29 17:02:48 -0400

    Using let and const...

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -1,8 +1,16 @@
 function startGame(_v: any) {
   _v.preventDefault();
-  // starting a new game
-  var messagesElement = document.getElementById('messages');
-  messagesElement!.innerText = 'Welcome to MultiMath! Starting new game...';  
+
+  const playerName: string = 'Fast Junkie...';
+  logPlayer(playerName);
+
+  const messagesElement: HTMLElement | null = document.getElementById('messages');
+  messagesElement!.innerText = 'Welcome to MultiMath! Starting a new game...';  
+}
+
+function logPlayer(...args: any[]): void {
+  const [name] = args;
+  console.debug('New game started for player: %s', name);
 }
 
 document.getElementById('start-game')!.addEventListener('click', startGame);
