5dcccb318a5d047942338d9565ea69dd6e1dec1f
Fast Junkie <fast-junkie@pm.me>
2023-09-29 15:46:22 -0400

    In the beginning...

--- public/assets/js/app.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-function startGame(_v) {
-    _v.preventDefault();
-    // starting a new game
-    var messagesElement = document.getElementById('messages');
-    messagesElement.innerText = 'Welcome to MultiMath! Starting new game...';
-}
-document.getElementById('start-game').addEventListener('click', startGame);
