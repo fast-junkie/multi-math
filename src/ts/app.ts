393f917f8e6f8f48b71b2b0fac8a95015eaac675
Fast Junkie <fast-junkie@pm.me>
2023-10-02 12:04:07 -0400

    Completed module 04...

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -1,16 +1,35 @@
-function startGame(_v: any) {
+function startGame(_v: any): void {
   _v.preventDefault();
 
-  let playerName: string = 'Fast Junkie...';
+  const playerName: string | undefined = getInputValue('player-name');
   logPlayer(playerName);
+
+  postScore(-1, playerName);
 
   const messagesElement: HTMLElement | null = document.getElementById('messages');
   messagesElement!.innerText = 'Welcome to MultiMath! Starting a new game...';  
 }
 
-function logPlayer(...args: any[]): void {
-  const [name] = args;
-  console.debug('New game started for player: %s', name);
+function logPlayer(_name: string = 'Fast Junkie...'): void {
+  console.debug('New game started for player: %s', _name);
 }
+
+function postScore(_score: number, _playerName: string = 'Fast Junkie...'): void {
+  let logger: (value: string) => void;
+  logger = (_score < 0) ? logError : logMessage;
+
+  const elem: HTMLElement | null = document.getElementById('posted-scores'); 
+  elem!.innerText = `${_score} - ${_playerName}`;
+
+  logger(`Score: ${_score}`);
+}
+
+function getInputValue(selector: string): string | undefined {
+  const elem: HTMLInputElement = <HTMLInputElement>document.getElementById(selector);
+  return (elem.value !== '') ? elem.value : undefined;
+}
+
+const logMessage = (message: string): void => console.debug(message);
+const logError = (error: string): void => console.error(error);
 
 document.getElementById('start-game')!.addEventListener('click', startGame);
